9e811eb20395bfbf8af47737367e8010ebd14e7f
AsgrimS <jan.semik@gmail.com>
2023-04-03 16:52:34 +0200

    show dot files in neo tree

--- plugins/core.lua
+++ plugins/core.lua
@@ -74,4 +74,18 @@ return {
   --     }, { mode = "n", prefix = "<leader>" })
   --   end,
   -- },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".git",
+          },
+        },
+      },
+    },
+  },
 }
