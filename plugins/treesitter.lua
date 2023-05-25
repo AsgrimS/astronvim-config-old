7a717bda6992cc957ffaffa11729bd9c473e5e9d
AsgrimS <jan.semik@gmail.com>
2023-05-25 11:45:48 +0200

    tresitter modules

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -6,6 +6,7 @@ return {
       "python",
       "javascript",
       "typescript",
+      "tsx",
       "svelte",
       "rust",
       "json",
@@ -14,6 +15,7 @@ return {
       "scss",
       "yaml",
       "prisma",
+      "toml",
     },
   },
 }
